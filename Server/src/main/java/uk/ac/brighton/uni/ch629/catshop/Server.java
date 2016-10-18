636b30ca7cd84ef739d856f65081a8fd023843ef
Charlie Howes <Zurrox@live.com>
2016-10-18 22:18:47 +0100

    Added routes to access product data as JSON.

--- Server/src/main/java/uk/ac/brighton/uni/ch629/catshop/Server.java
+++ Server/src/main/java/uk/ac/brighton/uni/ch629/catshop/Server.java
@@ -17,27 +17,45 @@ public class Server {
     public static void main(String[] args) {
         String userName = "root", password = "pass", url = "jdbc:mysql://localhost:3306/catshop";
 
-        try(Connection conn = DriverManager.getConnection(url, userName, password)) {
-            DSLContext create = DSL.using(conn, SQLDialect.MYSQL);
-            Result<Record> result = create.select().from(Product.PRODUCT).fetch();
+        get("/product/all", (req, res) -> {
+            try (Connection conn = DriverManager.getConnection(url, userName, password)) {
+                DSLContext create = DSL.using(conn, SQLDialect.MYSQL);
+                Result<Record> result = create.select().from(Product.PRODUCT).fetch();
 
-            for(Record r : result) {
-                Integer productNumber = r.getValue(Product.PRODUCT.PRODUCT_NUMBER);
-                String description = r.getValue(Product.PRODUCT.DESCRIPTION);
-                System.out.println(productNumber + ": " + description);
+                JsonObject jsonObject = new JsonObject();
+                for (Record r : result) {
+                    JsonObject object = new JsonObject();
+                    object.addProperty("description", r.getValue(Product.PRODUCT.DESCRIPTION));
+                    object.addProperty("image", r.getValue(Product.PRODUCT.IMAGE));
+                    object.addProperty("stock", r.getValue(Product.PRODUCT.STOCK));
+                    jsonObject.add(r.getValue(Product.PRODUCT.PRODUCT_NUMBER).toString(), object);
+                }
+                return jsonObject;
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        }catch (Exception e) {
-            e.printStackTrace();
-        }
+            return "";
+        });
 
-        get("/", (req, res) -> {
-            JsonObject jsonObject = new JsonObject();
-            JsonObject insideObject = new JsonObject();
-            insideObject.addProperty("IP Address", req.ip());
-            insideObject.addProperty("name", "Hello");
-            jsonObject.add("Test", insideObject);
-            jsonObject.addProperty("poop", true);
-            return jsonObject;
+        get("/product/:id", (req, res) -> {
+            try (Connection conn = DriverManager.getConnection(url, userName, password)) {
+                DSLContext create = DSL.using(conn, SQLDialect.MYSQL);
+                Result<Record> result = create.select().from(Product.PRODUCT).where(Product.PRODUCT.PRODUCT_NUMBER.equal(Integer.valueOf(req.params(":id")))).fetch();
+
+                JsonObject jsonObject = new JsonObject();
+                for (Record r : result) {
+                    JsonObject object = new JsonObject();
+                    object.addProperty("description", r.getValue(Product.PRODUCT.DESCRIPTION));
+                    object.addProperty("image", r.getValue(Product.PRODUCT.IMAGE));
+                    object.addProperty("stock", r.getValue(Product.PRODUCT.STOCK));
+                    return object;
+//                    jsonObject.add(r.getValue(Product.PRODUCT.PRODUCT_NUMBER).toString(), object);
+                }
+                return jsonObject;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return "";
         });
     }
 }
