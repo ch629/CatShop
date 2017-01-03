f4e218f055444d604e50afa1a5991ef9a92efb1a
ch629 <zurrox@live.com>
2017-01-03 14:32:26 +0000

    Updated to try Google's MultiMap. (Could potentially use  Google Guava Caching for the images on the client)

--- Server/src/main/java/uk/ac/brighton/uni/ch629/catshop/connections/SubscriptionManager.java
+++ Server/src/main/java/uk/ac/brighton/uni/ch629/catshop/connections/SubscriptionManager.java
@@ -1,5 +1,8 @@
 package uk.ac.brighton.uni.ch629.catshop.connections;
 
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Multimaps;
 import uk.ac.brighton.uni.ch629.catshop.update.Update;
 
 import java.util.*;
@@ -8,8 +11,10 @@ import java.util.concurrent.ConcurrentSkipListSet;
 public class SubscriptionManager {
     public static final SubscriptionManager INSTANCE = new SubscriptionManager();
     private static final int TIMEOUT = 120;
+    @Deprecated //TODO: Might need to keep this dependant on concurrency
     private final Map<Class<? extends Update>, List<Subscription>> subscriptions = new HashMap<>(); //TODO: Might need a Queue for tasks (LIFO)
     private final Set<Subscription> responses = new ConcurrentSkipListSet<>();
+    private final Multimap<Class<? extends Update>, Subscription> subscriptionsNew = HashMultimap.create(); //HashSet values
 
     private SubscriptionManager() {
     }
@@ -20,10 +25,19 @@ public class SubscriptionManager {
 
     public void addSubscription(Subscription subscription) {
         Arrays.stream(subscription.getTypes()).forEach(type -> subscriptions.get(type).add(subscription));
+        Arrays.stream(subscription.getTypes()).forEach(type -> subscriptionsNew.put(type, subscription));
     }
 
     private boolean removeSubscriptions(String ip, int port) {
         boolean ret = false;
+
+        synchronized (subscriptionsNew) {
+            Multimap<Class<? extends Update>, Subscription> filteredSubs = Multimaps.filterEntries(subscriptionsNew, sub -> {
+                assert sub != null;
+                return sub.getValue().getIP().equals(ip) && sub.getValue().getPort() == port;
+            });
+            filteredSubs.entries().forEach(entry -> subscriptionsNew.remove(entry.getKey(), entry.getValue()));
+        }
 
         synchronized (subscriptions) {
             for (Class<? extends Update> updateClass : subscriptions.keySet()) {
@@ -37,6 +51,10 @@ public class SubscriptionManager {
 
     private boolean removeSubscription(Subscription subscription) {
         boolean ret = false;
+
+        synchronized (subscriptionsNew) {
+            Arrays.stream(subscription.getTypes()).forEach(subType -> subscriptionsNew.remove(subType, subscription));
+        }
 
         synchronized (subscriptions) {
             for (Class<? extends Update> clazz : subscriptions.keySet()) {
@@ -65,6 +83,10 @@ public class SubscriptionManager {
     }
 
     private void sendUpdate(Update update) {
+        synchronized (subscriptionsNew) {
+            subscriptionsNew.get(update.getType()).forEach(subscription -> subscription.sendUpdate(update));
+        }
+
         synchronized (subscriptions) {
             List<Subscription> subs = subscriptions.get(update.getType());
             subs.forEach(sub -> sub.sendUpdate(update));
