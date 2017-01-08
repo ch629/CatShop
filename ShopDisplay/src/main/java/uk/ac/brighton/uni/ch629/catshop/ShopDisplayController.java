dc66ae7f85f69c5f20355719ae1eb5d0d94c8cbc
Charlie Howes <Zurrox@live.com>
2017-01-08 20:25:11 +0000

    Did some work with gradle, but I can't get spring boot to work in the jar.

--- ShopDisplay/src/main/java/uk/ac/brighton/uni/ch629/catshop/ShopDisplayController.java
+++ ShopDisplay/src/main/java/uk/ac/brighton/uni/ch629/catshop/ShopDisplayController.java
@@ -1,4 +1,20 @@
 package uk.ac.brighton.uni.ch629.catshop;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
 public class ShopDisplayController {
+    private ShopDisplayModel model;
+
+    @FXML
+    private ListView listView;
+
+    public ShopDisplayController(ShopDisplayModel shopDisplayModel) {
+        this.model = shopDisplayModel;
+    }
+
+    @FXML
+    private void initialize() {
+
+    }
 }
