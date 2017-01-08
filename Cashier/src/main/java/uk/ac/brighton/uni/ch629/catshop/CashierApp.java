18ca5f4d304be7d87fd22c59a61a80ee1d4e5b39
Charlie Howes <Zurrox@live.com>
2017-01-08 17:05:35 +0000

    Start of Cashier client.

--- Cashier/src/main/java/uk/ac/brighton/uni/ch629/catshop/CashierApp.java
+++ Cashier/src/main/java/uk/ac/brighton/uni/ch629/catshop/CashierApp.java
@@ -1,11 +1,21 @@
 package uk.ac.brighton.uni.ch629.catshop;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class CashierApp extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/Cashier.fxml"));
+        loader.setControllerFactory(t -> new CashierController(new CashierModel()));
 
+        primaryStage.setScene(new Scene(loader.load()));
+        primaryStage.show();
     }
 }
--- Cashier/src/main/java/uk/ac/brighton/uni/ch629/catshop/CashierController.java
+++ Cashier/src/main/java/uk/ac/brighton/uni/ch629/catshop/CashierController.java
@@ -1,4 +1,56 @@
 package uk.ac.brighton.uni.ch629.catshop;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import uk.ac.brighton.uni.ch629.catshop.data.Product;
+import uk.ac.brighton.uni.ch629.catshop.data.RequestUtil;
+
 public class CashierController {
+    private CashierModel model;
+
+    @FXML
+    private TextField search;
+
+    @FXML
+    private Label productName;
+
+    @FXML
+    private TableView basket;
+
+    @FXML
+    private Label totalPrice;
+
+    public CashierController(CashierModel cashierModel) {
+        this.model = cashierModel;
+    }
+
+    @FXML
+    private void initialize() {
+
+    }
+
+    @FXML
+    public void onCheck(ActionEvent event) {
+        Product product = RequestUtil.getProduct(Integer.parseInt(search.getText()));
+        if (product != null) productName.setText(product.getDescription());
+        else productName.setText("No Product Found");
+    }
+
+    @FXML
+    public void onAdd(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void onBuy(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void onClear(ActionEvent event) {
+
+    }
 }
